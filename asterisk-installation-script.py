441171cde7ec5e77d83921ff4e6ecf867f007af2
csegura2011 <cristian.segura.lepe@gmail.com>
2014-11-08 20:16:48 -0300

    comments added

--- asterisk-installation-script.py
+++ asterisk-installation-script.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+#####################################################################
+# Author: Cristian Segura L.                                        #
+# Email: cristian+dot+segura+dot+lepe+arroba+dot+gmail+dot+com      #
+# Creation Date: Sat nov 8 20:11:38 CLST 2014                       #
+# Version: 0.1                                                      #
+# License: GPL v2.0 (check LICENSE file)                            #
+# Usage: Installation of Asterisk IP PBX                            #
+#####################################################################
 
 lstArch = [
 	"amd64", "x86_64", "i386", "i686"
@@ -10,4 +18,8 @@ lstPkgs = {
 	'i386': 'mv nmap tcpdump gcc gcc-g++ ',
 	'i686': 'mv nmap tcpdump gcc gcc-g++ '
 }
+
+
+
+
 
