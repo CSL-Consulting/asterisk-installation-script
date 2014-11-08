97ae1532a08f961bcc75f15876f4be22c08108f3
csegura2011 <cristian.segura.lepe@gmail.com>
2014-11-08 20:08:41 -0300

    test.sh was deleted

---
+++ asterisk-installation-script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+lstArch = [
+	"amd64", "x86_64", "i386", "i686"
+]
+
+lstPkgs = {
+	'amd64': 'mv nmap tcpdump gcc gcc-g++ ',
+	'x86_64': 'mv nmap tcpdump gcc gcc-g++ ',
+	'i386': 'mv nmap tcpdump gcc gcc-g++ ',
+	'i686': 'mv nmap tcpdump gcc gcc-g++ '
+}
+
