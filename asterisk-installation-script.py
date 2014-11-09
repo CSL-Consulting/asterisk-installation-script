9340587d0e1e9c7ff7767568c08a81f821588b2d
csegura2011 <cristian.segura.lepe@gmail.com>
2014-11-08 21:31:15 -0300

    create work dir using datetime, download asterisk

--- asterisk-installation-script.py
+++ asterisk-installation-script.py
@@ -7,52 +7,68 @@
 # Version: 0.1                                                      #
 # License: GPL v2.0 (check LICENSE file)                            #
 # Usage: Installation of Asterisk IP PBX                            #
+# Dependencies:                                                     #
+#  + Python 2.7                                                     #
+#  + wget                                                           #
+# Tested on :                                                       #
+#  + Ubuntu Desktop 13.10 amd64                                     #
 #####################################################################
 
-
 import subprocess
-
-#lstArch = [
-#	"amd64", "x86_64", "i386", "i686"
-#]
-#
-#lstPkgs = {
-#	'amd64': 'mv nmap tcpdump gcc gcc-g++ ',
-#	'x86_64': 'mv nmap tcpdump gcc gcc-g++ ',
-#	'i386': 'mv nmap tcpdump gcc gcc-g++ ',
-#	'i686': 'mv nmap tcpdump gcc gcc-g++ '
-#}
+import time
+import datetime
+import os
 
 
-#subprocess.call(
-#	args,
-#	*, 
-#	stdin=None, 
-#	stdout=None,
-#	stderr=None,
-#	shell=False
-#)
+# Create working directory to download source code
+
+nowDateTime = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+wrkDir = "%s-asterisk11-sources" % ( nowDateTime )
+
+if not os.path.exists( wrkDir ):
+	os.makedirs( wrkDir )
+else:
+	print "[+] ERROR: Cannot create directory %s" % (wrkDir)
+	exit -1
+
+#print str(os.path) + wrkDir
+# print str(os.getcwd()) + '/' + wrkDir
+print "[+] Changing to directory %s" % (wrkDir)
+os.chdir( os.getcwd() + '/' + wrkDir )
 
 
+# Downloading Files
 
-fname = ""
 baseURL = "http://downloads.asterisk.org/pub/telephony"
 
-lPriDir = "libpri"
-lPriTgzFile = "libpri-1.4-current.tar.gz"
-lPriDownPath = baseURL + '/' + lPriDir + '/' + lPriTgzFile
+lAstDir = "asterisk"
+lAstTgzFile = "asterisk-11-current.tar.gz"
+lAstDownPath = baseURL + '/' + lAstDir + '/' + lAstTgzFile
 
 lPriDir = "libpri"
 lPriTgzFile = "libpri-1.4-current.tar.gz"
 lPriDownPath = baseURL + '/' + lPriDir + '/' + lPriTgzFile
 
-print "==================================================="
-print "                                                   "
-print "   Downloading LIBPRI                              "
-print "                                                   "
-print "==================================================="
-print "[+]downloading file: %s using wget" % ( lPriDownPath )
+
+
+print ""
+print "*****************************************************"
+print "*                                                   *"
+print "*   Downloading LIBPRI                              *"
+print "*                                                   *"
+print "*****************************************************"
+print "[+]downloading file: %s using wget" % ( lPriTgzFile )
+print ""
 subprocess.call( ["wget", lPriDownPath] )
 
+print ""
+print "*****************************************************"
+print "*                                                   *"
+print "*   Downloading ASTERISK                            *"
+print "*                                                   *"
+print "*****************************************************"
+print "[+]downloading file: %s using wget" % ( lAstTgzFile )
+print ""
+subprocess.call( ["wget", lAstDownPath] )
 
 
